366dceb7995e1af56624bee158ef1c4e1d2ae657
gnumarcelo <gnumarcelo@gmail.com>
2011-03-27 11:59:54 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Megon::Application.config.secret_token = 'a0333f618f2805f81d3a4668b41dd5f3ac0126c910e9dadc6fc149dd81edf11d5015e8885e1261769cd9b6af564c03186cb1be9d0faa53eb44cec99510e477e1'
